f75dd54a7e599ae692947d466753fc28d09d8f39
tulintaspinar <tulinttaspinar@gmail.com>
2024-10-22 11:57:34 +0300

    product repository/service/dto added

---
+++ API/Elastichsearch/Elastichsearch.API/DTOs/ProductCreateDto.cs
@@ -0,0 +1,19 @@
+﻿
+using Elastichsearch.API.Model;
+
+namespace Elastichsearch.API.DTOs
+{
+    public record ProductCreateDto(string Name,decimal Price,int Stock, ProductFeatureDto Feature)
+    {
+        public Product CreateProduct()
+        {
+            return new Product
+            {
+                Name = Name,
+                Price = Price,
+                Stock = Stock,
+                Feature = new ProductFeature() { Width = Feature.Width, Height = Feature.Height, Color = Feature.Color }
+            };
+        }
+    }
+}
---
+++ API/Elastichsearch/Elastichsearch.API/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using Elastichsearch.API.DTOs;
+using Elastichsearch.API.Repositories;
+using System.Net;
+
+namespace Elastichsearch.API.Services
+{
+    public class ProductService
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductService(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<ResponseDto<ProductDto>> SaveAsync(ProductCreateDto request)
+        {
+            var responseProduct = await _productRepository.SaveAsync(request.CreateProduct());
+            if (responseProduct == null)
+                return ResponseDto<ProductDto>.Fail(new List<string> { "Kayıt esnasında bir hata meydana geldi." }, HttpStatusCode.InternalServerError);
+            return ResponseDto<ProductDto>.Success(responseProduct.CreateDto(),HttpStatusCode.Created);
+        }
+    }
+}
