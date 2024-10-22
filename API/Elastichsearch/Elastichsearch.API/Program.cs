a3b888337da85d18baf4445e3571d888d77735ca
tulintaspinar <tulinttaspinar@gmail.com>
2024-10-22 11:18:41 +0300

    elastich DI container added product and productfeature class added

--- API/Elastichsearch/Elastichsearch.API/Program.cs
+++ API/Elastichsearch/Elastichsearch.API/Program.cs
@@ -1,3 +1,7 @@
+using Elasticsearch.Net;
+using Nest;
+using Elastichsearch.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddElastic(builder.Configuration);
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
